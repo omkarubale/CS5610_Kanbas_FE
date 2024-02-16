f680e4fa309b69f45558bf1610ffb8646667ad47
Omkar Ubale <omkarubale@users.noreply.github.com>
2024-02-15 20:03:32 -0500

    A3: 2.7 highlight completed

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -4,6 +4,7 @@ import JavaScript from "./JavaScript";
 import Add from "./routing/Add";
 import PathParameters from "./routing/PathParameters";
 import ConditionalOutput from "./ConditionalOutput";
+import Highlight from "./Highlight";
 
 function Assignment3() {
   return (
@@ -14,6 +15,12 @@ function Assignment3() {
       <Classes />
       <Styles />
       <ConditionalOutput />
+      <Highlight>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga corrupti
+        non cumque hic quas officia aliquid nesciunt maxime labore, tempore
+        inventore sed explicabo facilis magni perferendis illum. Pariatur, quae
+        ipsam?
+      </Highlight>
     </div>
   );
 }
