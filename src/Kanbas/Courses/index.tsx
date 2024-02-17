43872841a0865950e1fd23931ff08742044d03d3
Omkar Ubale <omkarubale@users.noreply.github.com>
2024-02-17 18:54:47 -0500

    A3: 3.4 Courses Navigation completed

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,8 +1,9 @@
 import { courses } from "../../Kanbas/Database";
-import { useParams } from "react-router-dom";
+import { Navigate, Route, Routes, useParams } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import { FaBars } from "react-icons/fa6";
 import "./index.css";
+import CourseNavigation from "./Navigation";
 
 function Courses() {
   const { courseId } = useParams();
@@ -38,7 +39,28 @@ function Courses() {
         <hr />
       </div>
 
-      <div className="wd-after-breadcrumb"></div>
+      <div className="wd-after-breadcrumb">
+        <div className="wd-course-navigation-container">
+          <CourseNavigation />
+        </div>
+        <div className="wd-not-left-side flex-fill">
+          <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<h1>Home</h1>} />
+            <Route path="Modules" element={<h1>Modules</h1>} />
+            <Route path="Piazza" element={<h1>Piazza</h1>} />
+            <Route path="Assignments" element={<h1>Assignments</h1>} />
+            <Route
+              path="Assignments/:assignmentId"
+              element={<h1>Assignment Editor</h1>}
+            />
+            <Route path="Grades" element={<h1>Grades</h1>} />
+            {/* Inside each, we need this structure:
+              <div className="middle-content flex-fill"></div>
+              <div className="wd-right-side d-none d-lg-block"></div> */}
+          </Routes>
+        </div>
+      </div>
     </>
   );
 }
