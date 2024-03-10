710705009e0472559e046398984328e410517baa
Omkar Ubale <omkarubale@users.noreply.github.com>
2024-03-10 19:10:45 -0400

    A4: 2.5 completed Todo List for Redux

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -6,8 +6,12 @@ import PathParameters from "./routing/PathParameters";
 import ConditionalOutput from "./ConditionalOutput";
 import Highlight from "./Highlight";
 import TodoList from "./todos/TodoList";
+import { useSelector } from "react-redux";
+import { LabState } from "../store";
 
 function Assignment3() {
+  const { todos } = useSelector((state: LabState) => state.todosReducer);
+
   return (
     <div className="container">
       <h1>Assignment 3</h1>
@@ -23,6 +27,14 @@ function Assignment3() {
         ipsam?
       </Highlight>
       <TodoList />
+
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
