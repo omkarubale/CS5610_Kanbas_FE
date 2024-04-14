484f9903d4881fdec8343b4cdddabf304975b84e
Bharat Chawla <bharatchawla53@gmail.com>
2024-04-14 17:59:25 -0400

    MA and right side

--- src/Kanbas/Courses/Quizzes/Preview/QuestionLinks/index.tsx
+++ src/Kanbas/Courses/Quizzes/Preview/QuestionLinks/index.tsx
@@ -1,6 +1,48 @@
+import { Button } from "react-bootstrap";
+import { MdOutlineEdit } from "react-icons/md";
+import { Link, useParams } from "react-router-dom";
+import { GoQuestion } from "react-icons/go";
+import "./index.css";
+import { useSelector } from "react-redux";
+import { KanbasState } from "../../../../store";
+import { IKanbasQuizQuestion } from "../../../../store/interfaces/quizzes";
+import { useState } from "react";
+
 function QuizPreviewQuestionLinks() {
+    const { courseId, quizId } = useParams();
+    const [selectedIndex, setSelectedIndex] = useState<number>(0);
+
+    const handleQuestionClick = (index: number) => {
+        setSelectedIndex(index);
+    }
+
+    const quizQuestions: IKanbasQuizQuestion[] = useSelector(
+        (state: KanbasState) => state.quizzesReducer.quizQuestions
+    );
+
     return (
         <>
+            <Button className="wd-button-standard">
+                <Link to={`/Kanbas/Courses/${courseId}/Quizzes/${quizId}/edit`} className="button-sidebar-wide">
+                    <MdOutlineEdit />
+                    Keep Editing This Quiz
+                </Link>
+            </Button>
+            <br /><br />
+            <h3>Questions</h3>
+            <ul className="list-group wd-questions">
+                {quizQuestions?.map((question, index) => (
+                    <li
+                        className={`list-group-item ${index === selectedIndex ? 'active' : ''}`}
+                        key={index}
+                        onClick={() => handleQuestionClick(index)}>
+                        {/* TODO need to handle different links based on how questions are rendered */}
+                        <Link to="" ></Link>
+                        <GoQuestion className="mb-1 me-1" />
+                        Question {index + 1}
+                    </li>
+                ))}
+            </ul>
         </>
     );
 }
--- src/Kanbas/Courses/Quizzes/Preview/index.tsx
+++ src/Kanbas/Courses/Quizzes/Preview/index.tsx
@@ -5,22 +5,52 @@ import QuizQuestionPreviewSingle from "./Question/Single";
 import QuizQuestionPreviewList from "./Question/List";
 import MiddleContentActions from "../../../layout/Content/NotLeftSide/MiddleContent/MiddleContentActions";
 import MiddleContentData from "../../../layout/Content/NotLeftSide/MiddleContent/MiddleContentData";
+import { IoAlertCircleOutline } from "react-icons/io5";
+import { useSelector } from "react-redux";
+import { KanbasState } from "../../../store";
+import { Button } from "react-bootstrap";
+import "./index.css";
 
 function QuizPreview() {
 
-    // TODO this variable will be removed later to use props directly from JSON
-    const isQneQuestionAtATime = false;
+    const quizDetails = useSelector(
+        (state: KanbasState) => state.quizzesReducer.quiz
+    );
+
+    const handleSubmit = () => {
+
+    }
+
 
     return (
         <>
             <MiddleContent>
                 <MiddleContentActions>
-                    <></>
+                    <div className="d-flex justify-content-start align-items-center">
+                        <h2 className="fw-bold">{quizDetails?.title}</h2>
+                    </div>
+                    <div className="d-flex justify-content-start alert alert-danger" role="alert">
+                        <IoAlertCircleOutline className="fs-4 mb-1" />
+                        This is a preview of the published version of the quiz
+                    </div>
+                    <div className="d-flex justify-content-start">
+                        Started:
+                    </div>
+                    <div className="d-flex justify-content-start">
+                        <h3 className="fw-bold">Quiz Instructions</h3>
+                    </div>
                 </MiddleContentActions>
                 <hr />
                 <MiddleContentData>
-                    {isQneQuestionAtATime && QuizQuestionPreviewSingle}
-                    {isQneQuestionAtATime && QuizQuestionPreviewList}
+                    {quizDetails?.isOneQuestionAtATime
+                        ? <QuizQuestionPreviewSingle />
+                        : <QuizQuestionPreviewList />}
+
+                    <div className="form-actions">
+                        <Button type="submit" className="wd-button-standard" onClick={() => handleSubmit()}>
+                            Submit Quiz
+                        </Button>
+                    </div>
                 </MiddleContentData>
             </MiddleContent>
             <RightSide>
