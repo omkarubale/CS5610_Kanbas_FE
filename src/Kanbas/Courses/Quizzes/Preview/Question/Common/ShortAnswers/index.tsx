e1e1f6c1f302606bbb6cd1e42d0a70aa50b4f81a
Omkar Ubale <omkarubale@users.noreply.github.com>
2024-04-22 04:22:45 -0400

    FIX: added html parsing for question text in preview

--- src/Kanbas/Courses/Quizzes/Preview/Question/Common/ShortAnswers/index.tsx
+++ src/Kanbas/Courses/Quizzes/Preview/Question/Common/ShortAnswers/index.tsx
@@ -1,28 +1,28 @@
 import { Form } from "react-bootstrap";
 import "./index.css";
+import parse from "html-react-parser";
 
 function ShortAnswers({ question }: { question: any }) {
-    return (
-        <div className="wd-quiz-text">
-            <div className="wd-quiz-question-text">
-                <p>{question.questionText}</p>
-            </div>
-            <div className="wd-quiz-question-answers">
-                {question.correctBlankAnswers.map((choice: String, index: number) => (
-                    <div className="wd-quiz-question-answer" key={index}>
-                        <Form>
-                            <Form.Group className="mb-3">
-                                <Form.Label className="d-flex justify-content-start">
-                                    <Form.Control type="text">
-                                    </Form.Control>
-                                </Form.Label>
-                            </Form.Group>
-                        </Form>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="wd-quiz-text">
+      <div className="wd-quiz-question-text">
+        <p>{parse(question.questionText)}</p>
+      </div>
+      <div className="wd-quiz-question-answers">
+        {question.correctBlankAnswers.map((choice: String, index: number) => (
+          <div className="wd-quiz-question-answer" key={index}>
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label className="d-flex justify-content-start">
+                  <Form.Control type="text"></Form.Control>
+                </Form.Label>
+              </Form.Group>
+            </Form>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default ShortAnswers; 
+export default ShortAnswers;
